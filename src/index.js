485378109b0677ed7ea4aa59a3e7d38824a52f55
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-12-24 02:46:50 +0000

    Add example socket.io

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-const WebSocketServer = require('ws').Server
-const wss = new WebSocketServer({ port: 3000 })
+const http = require('http')
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const routes = require(path.join(__dirname, './routes'))
+const app = express()
 
-wss.on('connection', (ws) => {
-  ws.send('XYZ')
-  setInterval(() => ws.send((new Date).toLocaleTimeString()), 1000)
-  ws.on('message', (message) => console.log('received: %s', message))
+// view engine
+app.set('views', path.join(__dirname, './views'))
+app.set('view engine', 'pug')
+
+// add middlewares
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, './public')))
+
+// add routes
+app.use('/', routes)
+
+// Define SocketIO
+const server = http.createServer(app)
+const io = require('socket.io').listen(server)
+
+io.sockets.on('connection', (socket) => {
+  socket.on('messageChange', (data) => {
+    console.log(data)
+    socket.emit('receive', data.message.split('').reverse().join(''))
+  })
+})
+
+app.set('port', process.env.port || 3000)
+server.listen(app.get('port'), () => {
+  console.log(`Express server listening on port ${app.get('port')}`)
 })
---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+const data = {
+  title: 'Socket App'
+}
+
+module.exports = (req, res, next) => res.render('index', data)
